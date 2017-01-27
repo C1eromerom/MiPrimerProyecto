a9548e30e1b9829468c38b3f011f913897a53310
eloy <eloy>
2017-01-27 17:19:00 +0100

    Ejercicio 2

--- src/Ejercicios/Racional.java
+++ src/Ejercicios/Racional.java
@@ -1,5 +1,35 @@
 package Ejercicios;
 
 public class Racional {
+	
+	private int numerador;
+	private int denominador;
+	
+	public Racional(int numer, int denom){
+		this.numerador=numer;
+		this.denominador=denom;
+	}
+	
+	public int getNumerador(){
+		return numerador;
+	}
+	
+	public void setNumerador(int numerador){
+		this.numerador=numerador;
+	}
+	
+	public int getDenominador(){
+		return denominador;
+	}
+	
+	public void setDenominador(int denominador){
+		this.denominador=denominador;
+	}
+	
+	public Racional multiplicacion (Racional c){
+		return new Racional(numerador*c.numerador , denominador*c.denominador);
+	}
+	
+	
 
 }
